5c8566df3c008053fd7b458ea0050cbe45cf4b8f
luis2605 <89785699+luis2605@users.noreply.github.com>
2022-08-31 13:52:35 +0200

    Update App.js

--- 4sandor/src/App.js
+++ 4sandor/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
